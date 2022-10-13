4e74973e63ac2d9e98318077ff3cee7e4866f202
Luke Clements <54648672+lukeclements@users.noreply.github.com>
2022-10-13 12:13:04 -1000

    Update tempertaure_data.ino

--- examples/temperature_data/tempertaure_data.ino
+++ examples/temperature_data/tempertaure_data.ino
@@ -1,41 +1,31 @@
-// Temperature readings from TMP36
+// Read temperature data from all seven TMP36 temperature sensors
 
-#define aref_voltage 3.3         
- 
-//TMP36 Pin Variables
-int tempPin = A0;        //the analog pin the TMP36's Vout (sense) pin is connected to
-                        //the resolution is 10 mV / degree centigrade with a
-                        //500 mV offset to allow for negative temperatures
-int tempReading;        // the analog reading from the sensor
- 
-void setup(void) {
-  // We'll send debugging information via the Serial monitor
-  Serial.begin(9600);   
+#define TEMPS_COUNT 7  // The total number of temperature sensors
+#define aref_voltage 3.3
+
+// Analog Pin temperature sensors are connected to
+const int TEMPS[TEMPS_COUNT] = { A0, A1, A6, A7, A8, A9, A17 };
+const char *temp_sen_names[TEMPS_COUNT] = { "obc_temp", "pdu_temp", "battery_board_temp", "solar_panel_1_temp", "solar_panel_2_temp", "solar_panel_3_temp", "solar_panel_4_temp" };
+int tempReading;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
 }
- 
-void loop(void) {
- 
-  tempReading = analogRead(tempPin);  
- 
-  Serial.print("Temp reading = ");
-  Serial.print(tempReading);     // the raw analog reading
- 
-  // converting that reading to voltage, which is based off the reference voltage
-  float voltage = tempReading * aref_voltage;
-  voltage /= 1024.0; 
- 
-  // print out the voltage
-  Serial.print(" - ");
-  Serial.print(voltage); Serial.println(" volts");
- 
-  // now print out the temperature in Fahrenheight
-  float temperatureF = (voltage * 1000) -58 ;  //converting from 10 mv per degree wit 500 mV offset
-                                               //to degrees ((volatge - 500mV) times 100)
-  Serial.print(temperatureF); Serial.println(" degrees F");
- 
-  // now convert to Celcius
-  float temperatureC = (temperatureF - 32) / 1.8;
-  Serial.print(temperatureC); Serial.println(" degrees C");
- 
-  delay(1000);
+
+void loop() {
+
+  // Temperature Sensors
+
+  float voltage[TEMPS_COUNT] = { 0 }, temperatureC[TEMPS_COUNT] = { 0 };
+
+  for (int i = 0; i < TEMPS_COUNT; i++) {
+    const int reading = analogRead(TEMPS[i]);  // getting the voltage reading from the temperature sensor
+    voltage[i] = reading * aref_voltage;       // converting that reading to voltage
+    voltage[i] /= 1024.0;
+    const float temperatureF = (voltage[i] * 1000) - 58;
+    temperatureC[i] = (temperatureF - 32) / 1.8;  // convert from fahrenheit to celsius
+    Serial.printf("Temperature %d: %.2f volts %.2f°C\n", i, voltage[i], temperatureC[i]);
+  }
+  delay(5000);
 }
